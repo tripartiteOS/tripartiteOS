2894d12e8c3ec64b727f7790ede8cddced59d8c4
ProximalElk6186 <133893464+ProximalElk6186@users.noreply.github.com>
2026-02-12 19:33:31 +0200

    Upd build script

--- buildAll.bat
+++ buildAll.bat
@@ -1,26 +1,9 @@
-rmdir build
-	cd src16
-		build.bat
-	cd ..
-	cd src32
-		build.bat
-	cd ..
-	cd src64
-		build.bat
-	cd ..
-mkdir build
-	cd build
-		mkdir TRPRTOS
-		cd TRPRTOS
-			mkdir SYSTEM
-		cd ..
-	cd ..
-cd src16
-	cd tos.com
-		copy tos.com ..\build\TRPRTOS\SYSTEM\
-	cd ..
-cd src32
-	copy kernel32.obj ..\build\TRPRTOS\SYSTEM\
-	cd ..
-cd src64 copy kernel64.obj ..\build\TRPRTOS\SYSTEM\
-REM Other stuff yet to do, I'm too lazy to write it rn
+:: tripartiteOS main build script
+:: I mean it should theoretically work
+@ECHO off
+ECHO ╔══════════════════════════════════════╗
+ECHO ║    tripartiteOS main build script    ║
+ECHO ╠══════════════════════════════════════╣
+ECHO ║ I mean, it should theoretically work ║
+ECHO ║ but no gurantees. Licensed under GPL ║
+ECHO ║            version 2 btw.            ║
